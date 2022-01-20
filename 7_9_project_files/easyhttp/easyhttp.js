2f566d6d06c2dd3e9aeb5b32c702979eccdec944
james a <jamesacoding@gmail.com>
2022-01-20 01:14:29 -0500

    easyhttp

---
+++ 7_9_project_files/easyhttp/easyhttp.js
@@ -0,0 +1,18 @@
+function easyHTTP() {
+  this.http = new XMLHttpRequest();
+}
+
+easyHTTP.prototype.get = function(url) {
+  this.http.open('GET', url, true);
+  let self = this;
+  this.http.onload = function() {
+    if(self.http.status === 200) {
+      console.log(self.http.responseText);
+    }
+  }
+  this.http.send();
+}
+
+
+
+
